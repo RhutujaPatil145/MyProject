a8b1c6966fcee4400ef4b3faae41149737c2b4da
unknown <rutupatil145@gmail.com>
2025-01-15 19:42:32 +0530

    Adding entire project at first

---
+++ src/main/java/com/achived/base/TestBase.java
@@ -0,0 +1,5 @@
+package com.achived.base;
+
+public class TestBase {
+
+}
---
+++ src/test/java/com/achived/testcase/FilterTest.java
@@ -0,0 +1,17 @@
+package com.achived.testcase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class FilterTest {
+	@Test
+	public void verrifyDesciptionofSearchResultofTshite(){
+		
+		WebDriver driver = (WebDriver) new ChromeDriver();
+		driver.get("https://www.myntra.com/");
+	
+	}
+
+}
